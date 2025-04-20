3060cb95da6a8a05689cdbbd1a8814fa172332d8
mansuri <161573889+msquareco@users.noreply.github.com>
2025-04-20 20:11:43 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,10 @@
+// main.js
 window.addEventListener("DOMContentLoaded", () => {
   fetch("/components/header.html")
     .then(res => res.text())
-    .then(data => {
-      document.getElementById("header-container").innerHTML = data;
-    });
+    .then(data => document.getElementById("header-container").innerHTML = data);
 
   fetch("/components/footer.html")
     .then(res => res.text())
-    .then(data => {
-      document.getElementById("footer-container").innerHTML = data;
-    });
+    .then(data => document.getElementById("footer-container").innerHTML = data);
 });
